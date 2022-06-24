11fcd106a69bdf671ec8ef8b2e4b020955b0b31e
Luis Gonzalez <fusion.platinum@gmail.com>
2022-06-24 13:01:59 +0100

    Instaall scripts

--- install.sh
+++ install.sh
@@ -5,14 +5,21 @@ branch_name="$1"
 github_repo=https://github.com/ucabreragonzalez/uScripts.git
 temp_path=$(mktemp -d)
 install_path=/var/opt/uScripts
+links_path=/usr/bin
 
 # main -------------------------------------------------
+
 # Clone repo to get the specified branch
 git clone $github_repo --branch $branch_name --single-branch $temp_path
 
+# Create install folder if does not exists
+if [ ! -d "$install_path" ]; then
+    mkdir -p "$install_path"
+fi
 
-alias create-folder="$temp_path/scripts/create-folder"
-alias test-folder="$temp_path/scripts/test-folder"
+# If previos scripts then delete
+find $install_path -maxdepth 1 -type f -delete
+# find /path/to/directory -xtype l -delete
 
-# cd $temp_path
-create-folder "$G_SCRIPTS_PATH"
+# Copy scripts
+cp $temp_path/* $install_path/
